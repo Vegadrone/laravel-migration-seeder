d2dc5b18ae17a9fa5df6305099ec58536362d928
Vegadrone <spenaantonino@gmail.com>
2022-09-09 18:05:19 +0200

    Print trains in page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,10 @@
     <title>Trains</title>
 </head>
 <body>
-
+@forelse ($trains as $train)
+    <h3>{{ $train->company }} | {{ $train->train_code }} | {{ $train->departure_station}} | {{ $train->departure_time}}  | {{ $train->arrival_station }} | {{ $train->arrival_time}} |  {{ $train->carriage_number}}</h3>
+@empty
+    <h2>Non ci sono treni</h2>
+@endforelse
 </body>
 </html>
